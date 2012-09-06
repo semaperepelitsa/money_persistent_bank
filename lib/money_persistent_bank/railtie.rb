40d230d9d693890e234ea4c0896bb8c9c2d16462
Semyon Perepelitsa <sema@sema.in>
2012-09-06 14:01:40 +0800

    Add middleware

--- lib/money_persistent_bank/railtie.rb
+++ lib/money_persistent_bank/railtie.rb
@@ -1,11 +1,26 @@
 module MoneyPersistentBank
   class Railtie < Rails::Railtie
-
     initializer "money_persistent_bank.make_default" do
       require "money"
       require "money/bank/persistent_bank"
       Money.default_bank = Money::Bank::PersistentBank.new(Rails.cache)
     end
 
+    initializer "money_persistent_bank.middleware" do
+      app.config.middleware.use Synchronization
+    end
+  end
+
+  # Updates rates from the cache on each request
+  class Synchronization
+    def initialize app, bank = Money.default_bank
+      @app = app
+      @bank = bank
+    end
+
+    def call env
+      @bank.import_rates
+      @app.call env
+    end
   end
 end

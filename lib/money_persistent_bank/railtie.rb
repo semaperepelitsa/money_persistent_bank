32fdbdde754721ad2987624366843f306a2dd212
Semyon Perepelitsa <sema@sema.in>
2012-09-06 13:47:27 +0800

    Update Railtie

--- lib/money_persistent_bank/railtie.rb
+++ lib/money_persistent_bank/railtie.rb
@@ -1,15 +1,10 @@
 module MoneyPersistentBank
   class Railtie < Rails::Railtie
 
-    initializer "money_persistent_bank.default_storage" do
-      require "money/bank/persistent_bank"
-      Money::Bank::PersistentBank.default_storage = Rails.cache
-    end
-
     initializer "money_persistent_bank.make_default" do
       require "money"
       require "money/bank/persistent_bank"
-      Money.default_bank = Money::Bank::PersistentBank.instance
+      Money.default_bank = Money::Bank::PersistentBank.new(Rails.cache)
     end
 
   end
